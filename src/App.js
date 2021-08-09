5209582f443a9cdeffcf984975281ffc7be9b557
acm0209 <acm0209@naver.com>
2021-08-09 12:33:38 +0900

    2.3 map Recap
    This reverts commit e9e72da52e5e817ee06b603bb7ceebd32ddfcf04.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react';
+
+function Food({name, picture}){
+  return (
+    <div>
+      <h1>I like {name}</h1>
+      <img src={picture} alt={name} />
+
+    </div>
+  )
+}
+
+const foodILike = [
+  {
+    id:1,
+  name: "Kimchi",
+  image: "https://kstory365.files.wordpress.com/2015/01/kimchi-01-cabbage.jpg"
+  },
+  {
+    id:2,
+  name: "bulgogi",
+  image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
+  },
+  {
+    id:3,
+  name: "kimbap",
+  image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
+  },
+  {
+    id:4,
+  name: "samgyetang",
+  image: "https://img.seoul.co.kr//img/upload/2019/07/25/SSI_20190725184016.jpg"
+  }
+];
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      {foodILike.map(dish => (<Food key={dish.id} name={dish.name} picture={dish.image} /> ))}
+      </div>
   );
 }
 
